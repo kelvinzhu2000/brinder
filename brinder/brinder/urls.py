2cac51d23b0c1c0ff52d0e7cca3af8c77ab3a308
kelvin <kelvin@wiredrive.com>
2013-10-11 18:37:31 -0700

    Adding better formatting for the privacy page and created the terms of service page

--- brinder/brinder/urls.py
+++ brinder/brinder/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'brinder.views.home', name='home'),
     url(r'^surveys/', include('surveys.urls')),
     url(r'^privacy/', views.privacy, name='privacy'),
+    url(r'^terms/', views.terms, name='terms'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
