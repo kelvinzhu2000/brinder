8bcdf3a898198083361c6bc54b75fa986e9a7bdc
Frederich <shaoyenc@usc.edu>
2014-02-11 19:29:56 -0800

    Update urls to enable registration

--- brinder/brinder/urls.py
+++ brinder/brinder/urls.py
@@ -15,11 +15,13 @@ urlpatterns = patterns('',
     url(r'^privacy/', views.privacy, name='privacy'),
     url(r'^terms/', views.terms, name='terms'),
     url(r'^contact/', views.contact, name='contact'),
-    url(r'^sendEmail/', sendEmail.sendToRecipients, name='sendEmail')
+    url(r'^sendEmail/', sendEmail.sendToRecipients, name='sendEmail'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/register/$', RegistrationView.as_view(), name='registration_register'),
+    url(r'^accounts/', include('registration.backends.simple.urls'))
 )
