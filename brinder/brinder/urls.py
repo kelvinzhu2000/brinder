23f7ce6dd8e5311a8ae2a666dc7cd9ccecf7695b
Chao Yan <AceYan@Chaos-MacBook-Air.local>
2014-02-11 17:58:19 -0800

    added basic email function

--- brinder/brinder/urls.py
+++ brinder/brinder/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from surveys import views
+from email import sendEmail
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -14,6 +15,7 @@ urlpatterns = patterns('',
     url(r'^privacy/', views.privacy, name='privacy'),
     url(r'^terms/', views.terms, name='terms'),
     url(r'^contact/', views.contact, name='contact'),
+    url(r'^sendEmail/', sendEmail.sendToRecipients, name='sendEmail')
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
